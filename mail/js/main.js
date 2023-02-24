4d76c17b764eacde477d9a713e618719dcac4b1e
FerrariMarc0 <marco1010@live.it>
2023-02-24 16:44:00 +0100

    mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,2 +1,35 @@
 
+'use strict';
 
+const casualMail= [
+    'alberto@gmail.com', 
+    'vittoria@live.it', 
+    'agatino@outlook.com', 
+    'gaetano@protonmail.com', 
+    'gae@outlook.com', 
+    'taniot@gmx.com', 
+    'giovanni@yahoo.com', 
+    'rosa@email.it', 
+    'Eugenia@libero.it', 
+    'giulia@gmail.com', 
+    'alex@live.it', 
+    'cribbio@outlook.com'
+]
+
+const userMail= prompt('Ineririsci la tua email:');
+let result= false;
+
+for( let i = 0; i < casualMail.length; i++){
+    
+    if(casualMail[i] === userMail){
+        result = true;
+    }
+}
+
+if(result === true){
+    console.log('La mail inserita è corretta!')
+    alert('La mail inserita è corretta!')
+} else{
+    console.log('Spiacente, la mail inserita non corrisponde, riprova.')
+    alert('Spiacente, la mail inserita non corrisponde, riprova.');
+}
