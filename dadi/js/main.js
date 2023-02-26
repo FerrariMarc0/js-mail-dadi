1b268c7245af0a072570e44a562f91019f6f48f5
FerrariMarc0 <marco1010@live.it>
2023-02-26 23:10:02 +0100

    loco dice layout

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,20 +1,32 @@
 'use strict';
 
-const userNum= Number(prompt('Scegli un numero da 1 a 6:'));
+const button= document.querySelector('button');
+const userNum= Math.round(Math.random() * 6);
 const comNum= Math.round(Math.random() * 6);
 
 console.log(userNum);
 console.log(comNum);
 
+button.addEventListener('click',
+function(){
+
+document.getElementById("user").innerHTML = userNum;
+document.getElementById("computer").innerHTML = comNum;
+
 if(userNum < comNum){
+    document.getElementById("result").innerText = ("you loose!");
     console.log('AHI, AHI, AHI...SPIACENTE, HAI PERSO! RITENTA.');
-    alert('AHI, AHI, AHI...SPIACENTE, HAI PERSO! RITENTA.');
+    
+    
 
 } else if(comNum < userNum){
+    document.getElementById("result").innerText = ("you win!!!");
     console.log('YEP!! COMPLIMENTI, HAI VINTO!!!');
-    alert('YEP!! COMPLIMENTI, HAI VINTO!!!');
+    
     
 } else{
     console.log("MANNAGGIA, MANNAGGIA! IL RISULTATO E' PARI...GIOCA DI NUOVO!");
-    alert("MANNAGGIA, MANNAGGIA! IL RISULTATO E' PARI...GIOCA DI NUOVO!");
+    document.getElementById("result").innerText = ("sorry, retry...");
 }
+
+})
